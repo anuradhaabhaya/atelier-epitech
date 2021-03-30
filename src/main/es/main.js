f47d5bce447928c0535ee7c8811b044a31ef907e
Anuradha Abhaya <anuradha.abhaya@free.fr>
2021-03-30 14:43:21 +0200

    désolé

--- src/main/es/main.js
+++ src/main/es/main.js
@@ -1,17 +1,25 @@
 export const rpn = {
-    additions(additions) {
-        return /* TODO: part 1 */undefined;
-    },
-    subtractions(subtractions) {
-        return /* TODO: part 2 */undefined;
-    },
-    multiplications(multiplications) {
-        return /* TODO: part 3 */undefined;
-    },
-    divisions(divisions) {
-        return /* TODO: part 4 */undefined;
-    },
-    compute(operations) {
-        return /* TODO: last part !! */undefined;
-    }
+  additions(additions) {
+    let total;
+      for (let i = 0; additions[i]; i++) {
+        if(additions[i] == '+') {
+            for (let y = i; additions[y] != ' '; y--) {
+                
+            }
+        }
+      }
+    return 1;
+  },
+  subtractions(subtractions) {
+    return /* TODO: part 2 */ undefined;
+  },
+  multiplications(multiplications) {
+    return /* TODO: part 3 */ undefined;
+  },
+  divisions(divisions) {
+    return /* TODO: part 4 */ undefined;
+  },
+  compute(operations) {
+    return /* TODO: last part !! */ undefined;
+  }
 };
